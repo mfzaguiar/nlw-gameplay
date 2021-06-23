223194a925dab00fb17f338fc8b3707a6eb9f146
Matheus Aguiar <first_mf@hotmail.com>
2021-06-23 18:02:35 -0300

    feat: add Fonts and Background component to wrap screens

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Image, StatusBar } from "react-native";
+import { View, Text, Image } from "react-native";
 
 import IllustrationImg from "../../assets/illustration.png";
 import { ButtonIcon } from "../../components/ButtonIcon";
@@ -8,11 +8,6 @@ import { styles } from "./styles";
 export function SignIn() {
   return (
     <View style={styles.container}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
       <Image
         source={IllustrationImg}
         style={styles.image}
@@ -20,11 +15,11 @@ export function SignIn() {
       />
       <View style={styles.content}>
         <Text style={styles.title}>
-          Organize{`\n`}suas jogatinas{`\n`}facílmente
+          Conecte-se{"\n"}e organize suas {"\n"}jogatinas
         </Text>
 
         <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games{`\n`}favoritos com seus amigos
+          Crie grupos para jogar seus games{"\n"}favoritos com seus amigos
         </Text>
         <ButtonIcon title="Entrar com Discord" activeOpacity={0.7} />
       </View>
